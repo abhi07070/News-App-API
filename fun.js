146a1cabd392331bbd37efa76c3fd07c9499cc66
Davinder Kumar <abhiverma97819@gmail.com>
2022-05-04 13:30:29 +0530

    first commit

---
+++ fun.js
@@ -0,0 +1,43 @@
+// Initialize the news api parameters
+let source = 'the-times-of-india';
+let apiKey = '225b63c8ad794e4ca99c4cd0d74b124c'
+
+// Grab the news container
+let newsAccordion = document.getElementById('newsAccordion');
+
+// Create an ajax get request
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
+
+// What to do when response is ready
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        console.log(articles);
+        let newsHtml = "";
+        articles.forEach(function(element, index) {
+            // console.log(element, index)
+            let news = `<div class="accordion-item my-3">
+            <h2 class="accordion-header" id="flush-heading${index}">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${index}" aria-expanded="false" aria-controls="flush-collapse${index}">
+                <b>Breaking News ${index+1}:</b> ${element["title"]}
+              </button>
+            </h2>
+            <div id="flush-collapse${index}" class="accordion-collapse collapse" aria-labelledby="flush-heading${index}" data-bs-parent="#newsAccordion">
+              <div class="accordion-body">${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a></div>
+            </div>
+          </div>`;
+            newsHtml += news;
+        });
+        newsAccordion.innerHTML = newsHtml;
+    }
+    else {
+        console.log("Some error occured")
+    }
+}    
+
+xhr.send()
+
+
+
